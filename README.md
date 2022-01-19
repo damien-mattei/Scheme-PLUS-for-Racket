efae5ffd93c8d49e92cf9a52881cb70fc9548a24
Damien MATTEI <damien.mattei@gmail.com>
2022-01-19 20:37:55 +0100

    test omit path

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Scheme-PLUS-for-Racket
 Scheme+ for Racket by Damien Mattei 
+Doc is almost the same of 
--- info.rkt
+++ info.rkt
@@ -1,6 +1,7 @@
 #lang info
 (define compile-omit-paths '("included-files" "required-files" "library" "examples" "SRFI" "compiled"))
-(define deps '("base"))
+(define test-omit-paths '("included-files" "required-files" "library" "examples" "SRFI" "compiled"))
+(define deps '("racket/base"))
 (define pkg-desc "Scheme+ for Racket and SRFI-105 curly-infix with REPL")
 (define version "1.0")
 (define pkg-authors '(mattei))
