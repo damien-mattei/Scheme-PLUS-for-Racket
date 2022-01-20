b277be25614efab0b3f43a5cee77b4efdf0a0ee9
Damien MATTEI <damien.mattei@gmail.com>
2022-01-20 04:46:42 +0100

    scribble

--- info.rkt
+++ info.rkt
@@ -1,7 +1,9 @@
 #lang info
+(define collection "scheme-plus")
 (define compile-omit-paths '("included-files" "required-files" "library" "examples" "SRFI" "compiled"))
 (define test-omit-paths '("included-files" "required-files" "library" "examples" "SRFI" "compiled"))
 (define deps '("racket/base"))
 (define pkg-desc "Scheme+ for Racket and SRFI-105 curly-infix with REPL")
 (define version "1.0")
 (define pkg-authors '(mattei))
+(define scribblings '(("scribblings/scheme-plus.scrbl" ())))
---
+++ scribblings/scheme-plus.scrbl
@@ -0,0 +1,7 @@
+#lang scribble/manual
+
+@title{Scheme+ for Racket}
+ 
+Welcome to my documentation,this documentation will be updated for RAcket later, for now you can use the Guile version:
+
+@hyperlink["https://github.com/damien-mattei/Scheme-PLUS-for-Guile/blob/main/README.md"]{Documentation of Scheme+ for Guile}
