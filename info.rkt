8257e283a6dc51dfb1670948885e24c929748fe0
Damien MATTEI <damien.mattei@gmail.com>
2022-11-18 21:18:52 +0100

    version 4 srfi  srfi-lib in deps

--- info.rkt
+++ info.rkt
@@ -3,10 +3,10 @@
 (define compile-omit-paths '("included-files" "required-files" "library" "examples" "SRFI" "compiled"))
 (define test-omit-paths '("included-files" "required-files" "library" "examples" "SRFI" "compiled"))
 (define deps '("racket/base"
-	       "srfi/31"))
+	       "srfi-lib"))
 (define pkg-desc "Scheme+ for Racket and SRFI-105 curly-infix with REPL")
 (define version "4.0")
 (define pkg-authors '(mattei))
 (define scribblings '(("scribblings/scheme-plus.scrbl" ())))
-(define build-deps '("scribble-lib" "racket-doc" "scribble-code-examples" "scribble-doc" "srfi-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "scribble-code-examples" "scribble-doc"))
 (define license 'LGPL-3.0-or-later)
