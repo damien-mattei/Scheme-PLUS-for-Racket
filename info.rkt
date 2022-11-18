86f7bbbcfc268884d0dc6ccad18b41f56c58ec41
Damien MATTEI <damien.mattei@gmail.com>
2022-11-18 21:24:42 +0100

    version 4 srfi  srfi-lib in deps

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 (define collection "Scheme-PLUS-for-Racket")
 (define compile-omit-paths '("included-files" "required-files" "library" "examples" "SRFI" "compiled"))
 (define test-omit-paths '("included-files" "required-files" "library" "examples" "SRFI" "compiled"))
-(define deps '("racket/base"
+(define deps '("base"
 	       "srfi-lib"))
 (define pkg-desc "Scheme+ for Racket and SRFI-105 curly-infix with REPL")
 (define version "4.0")
