238e5f17c07f45afd37ccce92354de0f60253d14
Damien MATTEI <damien.mattei@gmail.com>
2023-11-16 08:28:11 +0100

    r6rs-lib

--- info.rkt
+++ info.rkt
@@ -4,7 +4,8 @@
 (define test-omit-paths '("deprecated" "library" "examples" "SRFI" "compiled" "src"))
 (define deps '("base"
 	       "srfi-lib"
-	       "sci"))
+	       "sci"
+	       "r6rs-lib"))
 (define pkg-desc "Scheme+ for Racket and SRFI-105 curly-infix with REPL")
 (define version "7.2")
 (define pkg-authors '(mattei))
