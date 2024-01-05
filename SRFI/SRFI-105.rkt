b6fa81bcbdedf5df8f1a823797c9be2938bc4f6e
Damien MATTEI <damien.mattei@gmail.com>
2024-01-05 13:21:44 +0100

    aeration du code

--- SRFI/SRFI-105.rkt
+++ SRFI/SRFI-105.rkt
@@ -514,11 +514,14 @@
             ; In a real reader, consider handling "#! whitespace" per SRFI-22,
             ; and consider "#!" followed by / or . as a comment until "!#".
             ((char=? c #\!) (my-read port) (my-read port))
+	    
 	    ((char=? c #\;) (read-error "SRFI-105 REPL : Unsupported #; extension"))
+	    
 	    ;; read #:blabla
 	    ((char=? c #\:) (list->string
 			     (append (list #\# #\:)
 				     (read-until-delim port neoteric-delimiters))))
+	    
 	    ;; read #'blabla ,deal with syntax objects
 	    ;;((char=? c #\') (list 'syntax (curly-infix-read port)))
 	    ((char=? c #\') (list 'syntax (my-read port)))
