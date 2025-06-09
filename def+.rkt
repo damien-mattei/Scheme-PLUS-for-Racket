61ac64b03a4a1bf0afb6e5c48d88d291ff70de67
Damien MATTEI <damien.mattei@gmail.com>
2025-06-09 15:33:11 +0200

    removed infix and prefix now replaced by infix-prefix , added return and def+

---
+++ def+.rkt
@@ -0,0 +1,78 @@
+(module def+ racket/base
+
+
+	(provide def+ return return-rec) ; warning return and return-rec also provide elsewhere ! but it works....
+
+	(require srfi/31 ;; for 'rec in def*
+		 Scheme+/return
+		 racket/stxparam
+		 (for-syntax racket/base))
+
+
+;;(def+ (foo) (when (2 < 3) (return "vrai")))
+
+;;(foo)
+;;"vrai"
+
+(define-syntax def+
+
+  (lambda (stx)
+    
+      (syntax-case stx ()
+
+	;; multiple definitions without values assigned
+	;; (def+ (x y z))
+	;; TODO: remove? redundant with (declare x y z)
+	((_ (var1 ...)) #`(begin (define var1 '()) ...))
+	
+
+	((_ (<name> <arg> ...) <body> <body>* ...)
+	 
+         
+	 #'(define (<name> <arg> ...)
+
+	     (call/cc
+
+	      (lambda (ret-rec-id) ;(#,ret-rec-id)
+		;; In the body we adjust the 'return-rec' keyword so that calls
+		;; to 'return-rec' are replaced with calls to the escape
+		;; continuation.
+
+		(syntax-parameterize
+		 ([return-rec (syntax-rules ()
+				[(return-rec vals (... ...))
+				 (ret-rec-id vals (... ...))])])
+		 
+		 (apply (rec <name> (lambda (<arg> ...)
+				      
+				      (call/cc
+
+				       (lambda (ret-id) ;(#,ret-id)
+					 ;; In the body we adjust the 'return' keyword so that calls
+					 ;; to 'return' are replaced with calls to the escape
+					 ;; continuation.
+					 (syntax-parameterize
+					  ([return (syntax-rules ()
+						     [(return vals (... ...))
+						      (ret-id vals (... ...))])])
+					  ($nfx$ <body>)
+					  ($nfx$ <body>*)
+					  ...)))))
+			
+			(list <arg> ...)))))))
+
+	      
+
+	;; single definition without a value assigned
+	;; (def x)
+	((_ var) #`(define var '()))
+
+	;; (def x 7)
+	((_ var expr) #`(define var expr))
+
+	((_ err ...) #`(syntax-error "Bad def+ form"))
+
+	)))
+
+
+) ; end module
---
+++ return.rkt
@@ -0,0 +1,23 @@
+(module return racket/base
+
+
+	(provide return return-rec)
+
+	(require racket/stxparam
+		 (for-syntax racket/base))
+
+(define-syntax-parameter return-rec
+  ;;(lambda (stx)
+  (syntax-rules ()
+     ((_ . _)
+      (raise-syntax-error 'return-rec "can only be used inside def and def+")))
+  )
+
+(define-syntax-parameter return
+  ;;(lambda (stx)
+  (syntax-rules ()
+     ((_ . _)
+    (raise-syntax-error 'return "can only be used inside def and def+")))
+)
+
+)
