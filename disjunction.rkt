3ee0f5b4a862e35ab5eefeb48b1eb635e4052d79
Damien MATTEI <damien.mattei@gmail.com>
2025-11-29 18:13:28 +0100

    version 17.4

--- disjunction.rkt
+++ disjunction.rkt
@@ -1,9 +1,9 @@
 (module disjunction racket/base
 
 
-	(provide ∣∣ )
+	(provide ∣∣ )  ; this symbol is probably composed by other than | which is a reserved char in Racket, do not trust the apparence !
 
-	(define (∣∣ f . r)
+	(define (∣∣ f . r) ; there exist many vertical pipe in the charset !
 	     
 	   (cond ((null? r) f)
 		 (f #t)
